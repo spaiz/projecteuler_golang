77a8dd0627e1b493aef12d483c627e389aba9aaa
Alex Ravikovich <alex@guesty.com>
2020-12-17 00:03:06 +0200

    Added binary GCD implementation

--- 5/main.go
+++ 5/main.go
@@ -7,9 +7,9 @@ package main
 	What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 	https://projecteuler.net/problem=5
- */
+*/
 func main() {
-	
+
 }
 
 func SmallestMultiple(from, to int) int {
@@ -25,7 +25,7 @@ func SmallestMultiple(from, to int) int {
 
 func Dividable(num, from, to int) bool {
 	for i := from; i <= to; i += 1 {
-		if num % i != 0 {
+		if num%i != 0 {
 			return false
 		}
 	}
@@ -35,7 +35,7 @@ func Dividable(num, from, to int) bool {
 
 func SmallestMultiple2(from, to int) int {
 	res := 1
-	for i:= from; i <= to; i++ {
+	for i := from; i <= to; i++ {
 		res = lcm1(res, i)
 	}
 
@@ -56,8 +56,17 @@ func GCD1(a, b int) int {
 
 func SmallestMultiple3(from, to int) int {
 	res := 1
-	for i:= from; i <= to; i++ {
+	for i := from; i <= to; i++ {
 		res = lcm2(res, i)
+	}
+
+	return res
+}
+
+func SmallestMultiple4(from, to int) int {
+	res := 1
+	for i := from; i <= to; i++ {
+		res = lcm3(res, i)
 	}
 
 	return res
@@ -67,14 +76,53 @@ func lcm2(from int, to int) int {
 	return (from * to) / GCD2(from, to)
 }
 
+func lcm3(from int, to int) int {
+	return (from * to) / BinaryGCD(from, to)
+}
+
 func GCD2(a, b int) int {
 	for {
-			rem := a % b
-			if rem == 0 {
-				return b
-			}
+		rem := a % b
+		if rem == 0 {
+			return b
+		}
 
-			a = b
-			b = rem
+		a = b
+		b = rem
 	}
+}
+
+/*
+http://www.blackwasp.co.uk/SteinsAlgorithm.aspx
+*/
+func BinaryGCD(a, b int) int {
+	if a == b {
+		return a
+	}
+
+	if a == 0 {
+		return b
+	}
+
+	if b == 0 {
+		return a
+	}
+
+	if a%2 == 0 && b%2 == 0 {
+		return BinaryGCD(a>>1, b>>1) << 1
+	}
+
+	if a%2 == 0 {
+		return BinaryGCD(a>>1, b)
+	}
+
+	if b%2 == 0 {
+		return BinaryGCD(a, b>>1)
+	}
+
+	if a >= b {
+		return BinaryGCD((a-b)>>1, b)
+	}
+
+	return BinaryGCD(a, (b-a)>>1)
 }
--- 5/main_test.go
+++ 5/main_test.go
@@ -5,26 +5,32 @@ import (
 	"testing"
 )
 
-func TestSmallestMultiple1(t *testing.T) {
+func Test1_SmallestMultiple(t *testing.T) {
 	res := SmallestMultiple(1,10)
 	assert.Equal(t, 2520, res, "they should be equal")
 	t.Logf("\nResult: %d\n", res)
 }
 
-func TestSmallestMultiple2(t *testing.T) {
+func Test2_SmallestMultiple(t *testing.T) {
 	res := SmallestMultiple(1,20)
 	assert.Equal(t, 232792560, res, "they should be equal")
 	t.Logf("\nResult: %d\n", res)
 }
 
-func TestSmallestMultiple3(t *testing.T) {
+func Test1_SmallestMultiple2(t *testing.T) {
 	res := SmallestMultiple2(1,20)
 	assert.Equal(t, 232792560, res, "they should be equal")
 	t.Logf("\nResult: %d\n", res)
 }
 
-func TestSmallestMultiple4(t *testing.T) {
+func Test1_SmallestMultiple3(t *testing.T) {
 	res := SmallestMultiple3(1,20)
+	assert.Equal(t, 232792560, res, "they should be equal")
+	t.Logf("\nResult: %d\n", res)
+}
+
+func Test1_SmallestMultiple4(t *testing.T) {
+	res := SmallestMultiple4(1,20)
 	assert.Equal(t, 232792560, res, "they should be equal")
 	t.Logf("\nResult: %d\n", res)
 }
@@ -50,7 +56,15 @@ func Benchmark1_SmallestMultiple2(b *testing.B) {
 func Benchmark1_SmallestMultiple3(b *testing.B) {
 	var r int
 	for n := 0; n < b.N; n++ {
-		r = SmallestMultiple3(1, 20)
+		r = SmallestMultiple3(1, 40)
+	}
+	result = r
+}
+
+func Benchmark1_SmallestMultiple4(b *testing.B) {
+	var r int
+	for n := 0; n < b.N; n++ {
+		r = SmallestMultiple4(1, 40)
 	}
 	result = r
 }
